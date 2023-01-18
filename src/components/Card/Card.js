151a1d852509364bf06ebbcdccb0f366f5a9b95a
IVKrylova <ivk.lis@yandex.ru>
2023-01-18 17:18:19 +0300

    feat: add layout of byline

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -16,25 +16,28 @@ const Card = (props) => {
   return (
     <li className='card-item'>
       <div className='card'>
-      <p className='card__type'>{props.food.type}</p>
-      <h2 className='card__name'>{props.food.name}</h2>
-      <p className='card__testy'>{props.food.testy}</p>
-      <ul className='card__description'>
-        <li className='card__description-item'>
-          {packageQuantity}
-        </li>
-        <li className='card__description-item'>
-          {giftMouse}
-        </li>
-        <li className={cardDescriptionItemAdditionClass}>
-          {props.food.addition}
-        </li>
-      </ul>
-      <img className='card__img' src={props.food.img} alt={`${props.food.name} ${props.food.testy}`} />
-      <p className='card__weight'>
-        {props.food.weight} <span className='card__weight-unit'>кг</span>
-      </p>
+        <p className='card__type'>{props.food.type}</p>
+        <h2 className='card__name'>{props.food.name}</h2>
+        <p className='card__testy'>{props.food.testy}</p>
+        <ul className='card__description'>
+          <li className='card__description-item'>
+            {packageQuantity}
+          </li>
+          <li className='card__description-item'>
+            {giftMouse}
+          </li>
+          <li className={cardDescriptionItemAdditionClass}>
+            {props.food.addition}
+          </li>
+        </ul>
+        <img className='card__img' src={props.food.img} alt={`${props.food.name} ${props.food.testy}`} />
+        <p className='card__weight'>
+          {props.food.weight} <span className='card__weight-unit'>кг</span>
+        </p>
       </div>
+      <p className='byline'>
+        Чего сидишь? Порадуй котэ, <button className='byline__button' type='bytton'>купи.</button>
+      </p>
     </li>
   );
 }
